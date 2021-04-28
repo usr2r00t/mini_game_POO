294ae01f71dba8099606fd8c4401cdfbda28186b
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-28 20:42:16 +0200

    Update app_2.rb

--- app_2.rb
+++ app_2.rb
@@ -4,68 +4,64 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-#Intro du jeu
-puts "------------------------------------------------
-|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |
-|Le but du jeu est d'être le dernier survivant !|
--------------------------------------------------"
+#Acceuil
+puts "------------------------------------------------"
+puts "|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
+puts "|Le but du jeu est d'être le dernier survivant !|"
+puts "-------------------------------------------------"
 
 #Initialisation du joueur
-puts "Quel est ton prénom ?"
-print "> "
-human_player_name = gets.chomp
-user = HumanPlayer.new(human_player_name)
+puts "Choisi un nom de joueur :"
+print ">"
+username = gets.chomp
+human_player = HumanPlayer.new(username)
 
-#Initialisation des ennemis
-player1 = Player.new("Joe")
-player2 = Player.new("Jane")
-enemies = [player1, player2]
+#Initialisation des enemis
+enemies = []
+pnj_1 = Player.new("Josiane")
+enemies.push(pnj_1)
+pnj_2 = Player.new("José")
+enemies.push(pnj_2)
 
-#Combat
-while user.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
-  user.show_state
-
+#Le combat
+while human_player.life_points > 0 && (pnj_1.life_points > 0 || pnj_2.life_points > 0)
   puts " "
+  puts "-------------------------------------------------"
+  puts " "
+  human_player.show_state
+  puts " "
+  puts "-------------------------------------------------"
+  #Menu combat
   puts "Quelle action veux-tu effectuer ?"
-  puts " "
   puts "a - chercher une meilleure arme"
-  puts "s - chercher à se soigner"
-  puts " "
-  puts "attaquer un joueur en vue :"
-  i = 0
-  enemies.each do |enemy|
-    puts "#{i} - #{enemy.show_state}"
-    i += 1
-  end
-  print "> "
+  puts "s - chercher à se soigner" 
+  puts "Ou bien attaquer un joueur en vue :"
+  puts "0 - #{pnj_1.show_state}"
+  puts "1 - #{pnj_2.show_state}"
+  print ">"
   input = gets.chomp
-
-  case input
-  when 'a'
-    user.search_weapon
-  when 's'
-    user.search_health_pack
-  when '0'
-    user.attacks(player1)
-  when '1'
-    user.attacks(player2)
-  else 
-    "Selectionnez à nouveau"
+  puts " "
+  puts "-------------------------------------------------"
+  #Combat selon le choix du joueur
+  if input == "a"
+    human_player.search_weapon
+  elsif input == "s"
+    human_player.search_health_pack
+  elsif input == "0"
+    human_player.attacks(pnj_1)
+  else
+    human_player.attacks(pnj_2)
   end
-
-  #Tour des ennemis de riposter
-  puts "Les autres joueurs t'attaquent !"
+  #Combat, les pnj attaquent
+  puts "Les autres joueurs attaquent !"
   enemies.each do |enemy|
-    if enemy.life_points > 0
-      enemy.attacks(user)
-    end
+    enemy.attacks(human_player) if enemy.life_points > 0
   end
 end
 
 #Fin du jeu
-puts "La partie est finie"
-if user.life_points > 0
+if human_player.life_points > 0
   puts "BRAVO ! TU AS GAGNE !"
-else
+else 
   puts "Loser ! Tu as perdu !"
 end
