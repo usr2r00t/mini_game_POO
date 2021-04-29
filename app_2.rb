51f2487f21ed507dbac409064f0e06503ff229cc
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-29 11:54:54 +0200

    Update app_2.rb

--- app_2.rb
+++ app_2.rb
@@ -4,26 +4,25 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-#Acceuil
 puts "------------------------------------------------"
 puts "|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
 puts "|Le but du jeu est d'être le dernier survivant !|"
 puts "-------------------------------------------------"
 
-#Initialisation du joueur
+#Player_init
 puts "Choisi un nom de joueur :"
 print ">"
 username = gets.chomp
 human_player = HumanPlayer.new(username)
 
-#Initialisation des enemis
+#Enemy_init
 enemies = []
 pnj_1 = Player.new("Josiane")
 enemies.push(pnj_1)
 pnj_2 = Player.new("José")
 enemies.push(pnj_2)
 
-#Le combat
+#Fight
 while human_player.life_points > 0 && (pnj_1.life_points > 0 || pnj_2.life_points > 0)
   puts " "
   puts "-------------------------------------------------"
@@ -31,7 +30,7 @@ while human_player.life_points > 0 && (pnj_1.life_points > 0 || pnj_2.life_point
   human_player.show_state
   puts " "
   puts "-------------------------------------------------"
-  #Menu combat
+
   puts "Quelle action veux-tu effectuer ?"
   puts "a - chercher une meilleure arme"
   puts "s - chercher à se soigner" 
@@ -42,7 +41,7 @@ while human_player.life_points > 0 && (pnj_1.life_points > 0 || pnj_2.life_point
   input = gets.chomp
   puts " "
   puts "-------------------------------------------------"
-  #Combat selon le choix du joueur
+  
   if input == "a"
     human_player.search_weapon
   elsif input == "s"
@@ -52,14 +51,13 @@ while human_player.life_points > 0 && (pnj_1.life_points > 0 || pnj_2.life_point
   else
     human_player.attacks(pnj_2)
   end
-  #Combat, les pnj attaquent
+  #Fight, w/PNJ
   puts "Les autres joueurs attaquent !"
   enemies.each do |enemy|
     enemy.attacks(human_player) if enemy.life_points > 0
   end
 end
-
-#Fin du jeu
+#End_game
 if human_player.life_points > 0
   puts "BRAVO ! TU AS GAGNE !"
 else 
